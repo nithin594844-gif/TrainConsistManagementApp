6a58555780870464a0a021f7060222e45e6dfee4
nithin594844-gif <nithin.5948.44@gmail.com>
2026-04-14 11:41:21 +0530

    UC8: Filter Passenger Bogies Using Streams

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class TrainConsistManagementApp {
@@ -12,21 +11,20 @@ public class TrainConsistManagementApp {
         }
     }
     public static void main(String[] args) {
-        System.out.println("========================================");
-        System.out.println("UC7: Sort Bogies by Capacity(Comparator)");
-        System.out.println("========================================");
+        System.out.println("==========================================");
+        System.out.println("UC8: Filter Passenger Bogies Using Streams");
+        System.out.println("==========================================");
         List<Bogie> bogies = new ArrayList<>();
         bogies.add(new Bogie("Sleeper",72));
         bogies.add(new Bogie("AC Chair",56));
         bogies.add(new Bogie("First Class",24));
         bogies.add(new Bogie("General",90));
-        System.out.println("Before Sorting: ");
+        System.out.println("All Bogies: ");
         for(Bogie b : bogies)
             System.out.println(b.name + " -> " + b.capacity);
-        bogies.sort(Comparator.comparingInt(b -> b.capacity));
-        System.out.println("After Sorting by Capacity: ");
-        for(Bogie b : bogies)
-            System.out.println(b.name + " -> " + b.capacity);
-        System.out.println("UC7 sorting completed....");
+        System.out.println("Filtered Bogies (Capacity > 60): ");
+        bogies.stream().filter(b -> b.capacity > 60)
+                .forEach(b -> System.out.println(b.name + " -> " + b.capacity));
+        System.out.println("UC8 filtering completed....");
     }
 }
