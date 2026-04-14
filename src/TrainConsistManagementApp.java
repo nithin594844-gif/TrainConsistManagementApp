21487a1f7ddf086675840d5eb535db1344024555
nithin594844-gif <nithin.5948.44@gmail.com>
2026-04-14 18:27:04 +0530

    UC15: Safe Cargo Assignment Using try-catch-finally

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,37 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class TrainConsistManagementApp {
-    static class InvalidCapacityException extends Exception {
-        public InvalidCapacityException(String message) {
+    static class CargoSafetyException extends RuntimeException {
+        public CargoSafetyException(String message) {
             super(message);
         }
     }
-    static class PassangerBogie {
-        String type;
-        int capacity;
-        PassangerBogie(String type, int capacity) throws InvalidCapacityException {
-            this.type = type;
-            this.capacity = capacity;
-            if(capacity <= 0)
-                throw new InvalidCapacityException("Error: Capacity must be greater than zero");
+    static class GoodsBogie {
+        String shape;
+        String cargo;
+        GoodsBogie(String shape) {
+            this.shape = shape;
+        }
+        void assignCargo(String cargo) {
+            try {
+                if(shape.equals("Rectangular") && cargo.equals("Petroleum")) {
+                    throw new CargoSafetyException("Error: Unsafe cargo assignment!");
+                }
+                this.cargo = cargo;
+                System.out.println("Cargo assigned successfully -> " + cargo);
+                System.out.println("Cargo validation completed for " + shape + " bogie");
+            }
+            catch (CargoSafetyException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
     public static void main(String[] args) {
-        System.out.println("===================================");
-        System.out.println("UC14: Handle Invalid Bogie Capacity");
-        System.out.println("====================================");
-        List<PassangerBogie> bogies = new ArrayList<>();
-        try {
-            bogies.add(new PassangerBogie("AC Chair",-24));
-            bogies.add(new PassangerBogie("Sleeper",72));
-        }
-        catch(InvalidCapacityException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println("Created Bogies: ");
-        for(PassangerBogie b : bogies)
-            System.out.println(b.type + " -> " + b.capacity);
-        System.out.println("UC14 exception handling completed....");
+        System.out.println("===========================");
+        System.out.println("UC15: Safe Cargo Assignment");
+        System.out.println("===========================");
+        GoodsBogie g1 = new GoodsBogie("Cylindrical");
+        GoodsBogie g2 = new GoodsBogie("Rectangular");
+        g1.assignCargo("Petroleum");
+        g2.assignCargo("Petroleum");
+        System.out.println("UC15 runtime handling completed....");
     }
 }
