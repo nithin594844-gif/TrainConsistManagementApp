caf6b46230661f38374ea50854855290aceb6a27
nithin594844-gif <nithin.5948.44@gmail.com>
2026-04-14 09:54:12 +0530

    UC5: Preserve Insertion Order of Bogies (LinkedHashSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,26 +1,20 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        System.out.println("====================================");
-        System.out.println("UC4: Maintain Ordered Bogie Consist");
-        System.out.println("====================================");
-        List<String> trainConsist = new LinkedList<>();
-        trainConsist.addLast("Engine");
-        trainConsist.addLast("Sleeper");
-        trainConsist.addLast("AC");
-        trainConsist.addLast("Cargo");
-        trainConsist.addLast("Guard");
-        System.out.println("Initial Train Consist: ");
-        System.out.println(trainConsist);
-        trainConsist.add(2,"Pantry Car");
-        System.out.println("After Inserting 'Pantry Car' at position 2: ");
-        System.out.println(trainConsist);
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-        System.out.println("After Removing First and Last Bogie: ");
-        System.out.println(trainConsist);
-        System.out.println("UC4 ordered consist operations completed....");
+        System.out.println("=======================================");
+        System.out.println("UC5: Preserve Insertion Order of Bogies");
+        System.out.println("=======================================");
+        Set<String> formation = new LinkedHashSet<>();
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
+        System.out.println("Final Train Formation: ");
+        System.out.println(formation);
+        System.out.println("Note: \nLinkedHashSet preserves insertion order and removes duplicates automatically.");
+        System.out.println("UC5 formation setup completed....");
     }
 }
